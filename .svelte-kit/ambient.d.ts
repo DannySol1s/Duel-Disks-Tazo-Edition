9964ef9ba0738009c4ee680573805fd1c38ec7d6
Ángel Solís <137368175+DannySol1s@users.noreply.github.com>
2026-04-28 11:31:40 -0600

    chore: generate SvelteKit internal server files and environment type definitions

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -52,8 +52,9 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_5216_1592913036: string;
-	export const EFC_5216_344590478: string;
+	export const EFC_14312_1592913036: string;
+	export const FPS_BROWSER_APP_PROFILE_STRING: string;
+	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
 	export const GOPATH: string;
 	export const HOME: string;
@@ -222,8 +223,9 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_5216_1592913036: string;
-		EFC_5216_344590478: string;
+		EFC_14312_1592913036: string;
+		FPS_BROWSER_APP_PROFILE_STRING: string;
+		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
 		GOPATH: string;
 		HOME: string;
